c7403f6383a66aef277021b2610f93c5b7ef5030
James Hough <2jameshough@gmail.com>
2017-02-15 18:02:02 +0200

    Travis Build limited to supported frameworks

--- build.travis.sh
+++ build.travis.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build **/project.json
-
+dotnet restore && dotnet build Trifling.Common/project.json -f netstandard1.6 && dotnet build Trifling.Common.UnitTests/project.json -f netcoreapp1.0
